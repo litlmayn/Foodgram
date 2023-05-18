a381a449eaad2bce38c65d2cf79ba6b7880a33a5
Вадим Крамаренко <litlmayn@mail.ru>
2023-05-18 20:11:23 +0300

    v 1.4

--- backend/recipes/management/commands/importcsv.py
+++ backend/recipes/management/commands/importcsv.py
@@ -8,7 +8,7 @@ from recipes.models import Ingredient, Tag
 
 
 def get_reader(file_name):
-    csv_path = os.path.join(settings.BASE_DIR, '/data/', file_name)
+    csv_path = os.path.join(settings.BASE_DIR, 'data/', file_name)
     csv_file = open(csv_path, 'r', encoding='utf-8')
     return csv.reader(csv_file, delimiter=',')
 
